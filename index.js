20fc46cda8845c18d2a6c0e338bc60b83195630f
Jenia22f <ye.altukhov@gmail.com>
2021-03-10 16:48:04 +0200

    added first work code for chenge pixel and exif

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-const {readFile} = require('fs').promises;
-const getExif = require('get-exif');
-const modifyExif = require('modify-exif');
- 
+var piexif = require("piexifjs");
+var fs = require("fs");
+var Jimp = require('jimp');
+
 (async () => {
-  // The original photo is taken at 2017:11:19 08:47:19
-  const originalFile = await readFile('origin_1.jpg');
-  getExif(originalFile).Exif; //=> '2017:11:19 08:47:19'
- 
-  const newFile = modifyExif(await readFile('origin_1.jpg'), data => {
-    // 36867: tag ID of `DateTimeOriginal` tag
-    data.Exif['36867'] = '2018:06:15 12:00:00'
-  });
- 
-  getExif(newFile).Exif; //=> '2018:06:15 12:00:00'
-})();
+    var filename = "origin_1.jpg";
+
+    var jpeg = fs.readFileSync(filename);
+    var data = jpeg.toString("binary");
+
+    var zeroth = {};
+    var exif = {};
+    var gps = {};
+    zeroth[piexif.ImageIFD.Make] = "Make";
+    zeroth[piexif.ImageIFD.XResolution] = [777, 1];
+    zeroth[piexif.ImageIFD.YResolution] = [777, 1];
+    zeroth[piexif.ImageIFD.Software] = "Adobe Photoshop";
+    exif[piexif.ExifIFD.DateTimeOriginal] = "2010:10:10 10:10:10";
+    exif[piexif.ExifIFD.LensMake] = "LensMake";
+    exif[piexif.ExifIFD.Sharpness] = 777;
+    exif[piexif.ExifIFD.LensSpecification] = [[1, 1], [1, 1], [1, 1], [1, 1]];
+    gps[piexif.GPSIFD.GPSVersionID] = [7, 7, 7, 7];
+    gps[piexif.GPSIFD.GPSDateStamp] = "1999:99:99 99:99:99";
+    var exifObj = {"0th":zeroth, "Exif":exif, "GPS":gps};
+
+    console.log('exifObj', exifObj);
+
+    var exifbytes = piexif.dump(exifObj);
+
+    var newData = piexif.insert(exifbytes, data);
+    var newJpeg = Buffer.from(newData, "binary");
+
+    await cahngePixelColor(filename);
+    fs.writeFileSync(filename, newJpeg);
+})()
+
+
+
+async function cahngePixelColor(filename) {
+    const image = await Jimp.read(filename);
+    console.log('default pixel color', image.getPixelColor(1, 1));
+    image.setPixelColor(19027215, 1, 1);
+    console.log('changed pixel color', image.getPixelColor(1, 1));
+    image.write(filename);
+
+}
